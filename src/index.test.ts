34669367c046afc8b6c572b7ad7784d829735a22
Jyoti Puri <jyotipuri@gmail.com>
2022-10-26 21:03:16 +0400

    fix

--- src/index.test.ts
+++ src/index.test.ts
@@ -220,6 +220,6 @@ describe('retry logic in middleware connected to a port', () => {
       messageConsumer({
         method: RECONNECTED,
       });
-    }).toThrow();
+    }).toThrowError();
   });
 });
